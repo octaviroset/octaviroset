1bb171ae36c7ab7100c0d73d8a0aedcd5995ab29
octaviroset <octaviroset@gmail.com>
2019-03-12 19:35:10 +0100

    ASDAFSFWFFBYRGBC

---
+++ UF3.PR1/js/script.js
@@ -0,0 +1,76 @@
+$(document).ready(function(){
+    $( "#target" ).click(function() {
+  $(".titolprin")
+        .css("background-color","black")
+        .css("color","yellow")
+        .css("font-size", "2em")
+        .css("border" ,"2px solid yellow ");
+  $(".titolas").css("background-color","black")
+        .css("color","yellow")
+        .css("font-size", "2em")
+        .css("border" ,"2px solid yellow ");
+  $(".titoloffice").css("background-color","black")
+        .css("color","yellow")
+        .css("font-size", "2em")
+        .css("border" ,"2px solid yellow ");
+  $("header")
+        .css("background-color","black")
+        .css("color","yellow")
+        .css("font-size", "1em");
+   $("a")
+        .css("background-color","black")
+        .css("color","yellow")
+        .css("font-size", "2em")
+        .css("border" ,"2px solid yellow ");
+  $(".carta")
+        .css("background-color","black")
+        .css("color","yellow")
+        .css("font-size", "2em")
+        .css("border" ,"2px solid yellow ");
+    $("body")
+        .css("background-color","black");
+    $("p")
+        .css("color", "yellow");
+    $("h3")
+        .css("color", "yellow")
+        .css("color", "yellow");
+    $(".imatge")
+        .css("position", "relative")
+        .css("height", "auto")
+        .css("flex-direction", "column")
+        .css("display", "flex");
+    $(".carta")
+        .css("position", "relative")
+        .css("overflow", "visible");
+    $(".foto")
+        .css("position", "relative");
+    $(".text h2")
+        .css("color","yellow")
+        .css("line-height","70px")
+
+        .css("font-size", "3em");
+    $(".text h3")
+        .css("color","yellow")
+        .css("line-height","60px")
+
+        .css("font-size", "2em");
+    $(".text p")
+        .css("color","yellow")
+        .css("line-height","40px")
+
+        .css("font-size", "2em");
+
+
+
+});
+    $( "#target1" ).click(function() {
+  $( this ).toggleClass( "highlight" );
+         $(".titolprin")
+        .css("background-color","black")
+        .css("color","yellow")
+        .css("font-size", "2em")
+        .css("border" ,"2px solid yellow ")
+});
+
+    });
+
