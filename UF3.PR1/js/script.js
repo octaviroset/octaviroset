cd51f38270e9323d81a0d411ad1933ab0fdaffb5
octaviroset <octaviroset@gmail.com>
2019-03-26 19:07:45 +0100

    hola

--- UF3.PR1/js/script.js
+++ UF3.PR1/js/script.js
@@ -1,94 +1,94 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-
-
-    $( "#target" ).click(function() {
-  $(".titolprin")
-        .css("background-color","black")
-        .css("color","yellow")
-        .css("font-size", "2em")
-        .css("border" ,"2px solid yellow ");
-
-  $(".titolas")
-      .css("background-color","black")
-        .css("color","yellow")
-        .css("font-size", "2em")
-        .css("border" ,"2px solid yellow ");
-
-  $(".titoloffice")
-      .css("background-color","black")
-        .css("color","yellow")
-        .css("font-size", "2em")
-        .css("border" ,"2px solid yellow ");
-
-  $("header")
-        .css("background-color","black")
-        .css("color","yellow")
-        .css("font-size", "1em");
-
-   $("a")
-        .css("background-color","black")
-        .css("color","yellow")
-        .css("font-size", "2em")
-        .css("border" ,"2px solid yellow ");
-
-  $(".carta")
-        .css("background-color","black")
-        .css("color","yellow")
-        .css("font-size", "2em")
-        .css("position", "relative")
-        .css("overflow", "visible")
-        .css("border" ,"2px solid yellow ");
-
-    $("body")
-        .css("background-color","black");
-
-    $("p")
-        .css("color", "yellow");
-
-    $("h3")
-
-        .css("color", "yellow");
-
-    $(".imatge")
-        .css("position", "relative")
-
-        .css("flex-direction", "column")
-        .css("height", "auto")
-        .css("width", "400px")
-        .css("display", "flex");
-
-    $(".caixa  img")
-        .css("height", "400px")
-        .css("width", "400px");
-
-    $(".caixa")
-        .css("min-width", "410px");
-
-
-    $(".foto")
-        .css("position", "relative");
-    $(".text h2")
-        .css("color","yellow")
-        .css("line-height","70px")
-
-        .css("font-size", "3em");
-    $(".text h3")
-        .css("color","yellow")
-        .css("line-height","60px")
-
-        .css("font-size", "2em");
-     $(".text")
-         .css("margin-left", "60px");
-    $(".text p")
-        .css("color","yellow")
-        .css("line-height","40px")
-
-        .css("font-size", "2em");
-
-
-
-    });
+//
+//
+//    $("#target" ).click(function() {
+//  $(".titolprin")
+//        .css("background-color", "black")
+//        .css("color", "yellow")
+//        .css("font-size", "2em")
+//        .css("border", "2px solid yellow ");
+//
+//  $(".titolas")
+//      .css("background-color", "black")
+//        .css("color", "yellow")
+//        .css("font-size", "2em")
+//        .css("border", "2px solid yellow ");
+//
+//  $(".titoloffice")
+//      .css("background-color", "black")
+//        .css("color", "yellow")
+//        .css("font-size", "2em")
+//        .css("border", "2px solid yellow ");
+//
+//  $("header")
+//        .css("background-color", "black")
+//        .css("color", "yellow")
+//        .css("font-size", "1em");
+//
+//   $("a")
+//        .css("background-color", "black")
+//        .css("color", "yellow")
+//        .css("font-size", "2em")
+//        .css("border", "2px solid yellow ");
+//
+//  $(".carta")
+//        .css("background-color", "black")
+//        .css("color", "yellow")
+//        .css("font-size", "2em")
+//        .css("position", "relative")
+//        .css("overflow", "visible")
+//        .css("border", "2px solid yellow ");
+//
+//    $("body")
+//        .css("background-color", "black");
+//
+//    $("p")
+//        .css("color", "yellow");
+//
+//    $("h3")
+//
+//        .css("color", "yellow");
+//
+//    $(".imatge")
+//        .css("position", "relative")
+//
+//        .css("flex-direction", "column")
+//        .css("height", "auto")
+//        .css("width", "400px")
+//        .css("display", "flex");
+//
+//    $(".caixa  img")
+//        .css("height", "400px")
+//        .css("width", "400px");
+//
+//    $(".caixa")
+//        .css("min-width", "410px");
+//
+//
+//    $(".foto")
+//        .css("position", "relative");
+//    $(".text h2")
+//        .css("color", "yellow")
+//        .css("line-height", "70px")
+//
+//        .css("font-size", "3em");
+//    $(".text h3")
+//        .css("color", "yellow")
+//        .css("line-height", "60px")
+//
+//        .css("font-size", "2em");
+//     $(".text")
+//         .css("margin-left", "60px");
+//    $(".text p")
+//        .css("color", "yellow")
+//        .css("line-height", "40px")
+//
+//        .css("font-size", "2em");
+//
+//
+//
+//    });
 
      $( "#acces" ).click(function() {
         $( ".titolprin" ).toggleClass("titolprinacc");
@@ -101,8 +101,10 @@ $(document).ready(function(){
          $( "p" ).toggleClass("pacc");
          $( "h3" ).toggleClass("h3acc");
          $( ".imatge" ).toggleClass("imatgeacc");
-         $( ".caixa img" ).toggleClass("classeimg");
+         $( ".foto_cotxe" ).toggleClass("classeimg");
          $( ".caixa" ).toggleClass("caixaacc");
+
+         $( ".foto" ).toggleClass("fotoacc");
          $( ".text h2" ).toggleClass("classeh2");
          $( ".text h3" ).toggleClass("classeh3");
          $( ".text" ).toggleClass("textacc");
